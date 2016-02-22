01e2a20ab91dd825c2aa251a9a7792e4129378a7
Maya Gort <mgort@mymail.mines.edu>
2016-02-22 16:08:41 -0700

    fixed a thing

--- make_charts.js
+++ make_charts.js
@@ -1,3 +1,3 @@
-$.get("192.168.1.112/ApartmentTravelTimes-FrontEnd/get-all-times.php", function(data, status){
+$.get("get-all-times.php", function(data, status){
         alert("Data: " + data + "\nStatus: " + status);
     });
